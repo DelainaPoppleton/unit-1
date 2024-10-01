bd66fb2084c4d90b4b44c944cb295f5a127f037e
Delaina Poppleton <poppletd@wlhs.wlwv.k12.or.us>
2024-10-01 09:46:37 -0700

    Update Unit2HW1.py

--- Unit2HW1.py
+++ Unit2HW1.py
@@ -1,1 +1,18 @@
+'''
+Name: Delaina Poppleton
+Date: 10/1/24
+Description: Unit 2 homework 1
+'''
 
+print('Problem 1'.center(20,'-'))
+
+print('Problem 2'.center(20,'-'))
+
+print('Problem 3'.center(20,'-'))
+
+distance_seattle = 173.8
+miles_per_gallon = float(input("how many miles per gallon does your car get? "))
+price_of_gas = float(input("how much is a gallon of gas near your home? "))
+gallon_of_gas = float(input("how many gallons of gas does your car hold? "))
+
+miles_per_tank = miles_per_gallon*gallon_of_gas 
