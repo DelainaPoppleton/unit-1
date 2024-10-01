346b9e31af253218ffc27118f16e3a18073bb66f
Delaina Poppleton <poppletd@wlhs.wlwv.k12.or.us>
2024-10-01 11:14:45 -0700

    Update Unit2HW1.py

--- Unit2HW1.py
+++ Unit2HW1.py
@@ -6,6 +6,8 @@ Description: Unit 2 homework 1
 
 print('Problem 1'.center(20,'-'))
 
+
+
 print('Problem 2'.center(20,'-'))
 
 print('Problem 3'.center(20,'-'))
@@ -16,3 +18,14 @@ price_of_gas = float(input("how much is a gallon of gas near your home? "))
 gallon_of_gas = float(input("how many gallons of gas does your car hold? "))
 
 miles_per_tank = miles_per_gallon*gallon_of_gas 
+if miles_per_tank == 173.8:
+    total_cost = gallon_of_gas*price_of_gas 
+    print(f"The cost to drive to seattle is ${total_cost}")
+elif miles_per_tank > 173.8:
+     total_cost = gallon_of_gas*price_of_gas
+     print(f"The cost to drive to seattle is ${total_cost}")
+elif miles_per_tank < 173.8:
+    total_cost = gallon_of_gas*price_of_gas*2
+    print(f"The cost to drive to seattle is ${total_cost}")
+
+
